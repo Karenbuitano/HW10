a7ced27288762433d7dcdcb9d0336aa60303f10a
Karen Buitano <karenbuitano2016@u.northwestern.edu>
2015-06-10 13:21:06 -0500

    hello world demo

---
+++ Helloworld/app/src/androidTest/java/com/example/karenbuitano/helloworld/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.karenbuitano.helloworld;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by karenbuitano on 6/1/15.
+ */
+public class MainActivityTest extends TestCase {
+
+}
